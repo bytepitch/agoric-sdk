52b67cdfa2948a89122c06a68302dccc0d8a0b35
anilhelvaci <anilhelvaci96@gmail.com>
2024-10-14 20:11:07 +0300

    chore(acceptance): add tests for PSM to `z:acceptance`
    Refs: https://github.com/Agoric/BytePitchPartnerEng/issues/23
    
    fix(acceptance-psm): address change requests

--- a3p-integration/proposals/z:acceptance/test-lib/index.js
+++ a3p-integration/proposals/z:acceptance/test-lib/index.js
@@ -2,3 +2,16 @@ export const networkConfig = {
   rpcAddrs: ['http://0.0.0.0:26657'],
   chainName: 'agoriclocal',
 };
+
+/**
+ * Resolve after a delay in milliseconds.
+ *
+ * @param {number} ms
+ * @returns {Promise<void>}
+ */
+const delay = ms => new Promise(resolve => setTimeout(() => resolve(), ms));
+
+export const walletUtils = await makeWalletUtils(
+  { delay, execFileSync, fetch },
+  networkConfig,
+);
